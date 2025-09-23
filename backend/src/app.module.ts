8bef5218d59b108f2993369683deb7913bcc6dcc
elelmokao <show.999991@gmail.com>
2025-09-24 06:43:37 +0900

    feat: add postgresql docker-compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GeminiModule } from './gemini/gemini.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiKeysModule } from './db/api_keys/api_keys.module';
 
 @Module({
-  imports: [GeminiModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'gemini_chat_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GeminiModule,
+    ApiKeysModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
